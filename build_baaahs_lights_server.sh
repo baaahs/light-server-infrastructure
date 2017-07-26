1d28d2536150e30d8ddecacf9e79026590cacfbe
Josh Durbin <joshua.durbin@simplytapp.com>
2017-07-26 05:49:15 -0700

    add support for force re-install

--- build_baaahs_lights_server.sh
+++ build_baaahs_lights_server.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
+# force the re-installation of OLA using the force_reinstall_ola variable
+#ansible-playbook complete_playbook.yml -i inventory.dist --ask-vault-pass --extra-vars "force_reinstall_ola=true"
+
 ansible-playbook complete_playbook.yml -i inventory.dist --ask-vault-pass
