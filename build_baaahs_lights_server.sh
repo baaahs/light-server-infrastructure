d4dea81408b8aff06cbeee1c13c8fcbb42241622
Josh Durbin <durbinjo593@gmail.com>
2018-02-11 22:42:44 -0800

    adding timeout

--- build_baaahs_lights_server.sh
+++ build_baaahs_lights_server.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # force the re-installation of OLA using the force_reinstall_ola variable
-#ansible-playbook complete_playbook.yml -i inventory.dist --ask-vault-pass --extra-vars "force_reinstall_ola=true"
-
-ansible-playbook complete_playbook.yml -i inventory.dist --ask-vault-pass
+#ansible-playbook complete_playbook.yml -i inventory.dist --ask-vault-pass --extra-vars "force_reinstall_ola=true" --timeout=30
+#ansible-playbook complete_playbook.yml -i inventory.dist --ask-vault-pass --timeout=30
+g tim
+ansible-playbook complete_playbook.yml -i inventory.dist --vault-password-file=passwd --timeout=30
